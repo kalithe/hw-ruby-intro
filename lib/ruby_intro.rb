8325ef9bf95c41e607e1f66e1184c85cac8c268c
Jose Delgado <jdelgado@citadel.edu>
2016-09-10 17:58:34 +0000

    Finished all of Assignment 1!

--- lib/ruby_intro.rb
+++ lib/ruby_intro.rb
@@ -61,19 +61,38 @@ end
 # Part 2
 
 def hello(name)
-  # YOUR CODE HERE
+  str = "Hello, #{name}"
+  return str
 end
 
 def starts_with_consonant? s
-  # YOUR CODE HERE
+  if s !~ /^[aeiou\d\W]/i and not s.size == 0
+      return true
+  end
+  return false
 end
 
 def binary_multiple_of_4? s
-  # YOUR CODE HERE
+  if (s =~ /^[0-1]/) && (s.to_i(2) % 4 == 0)
+    return true
+  else 
+    return false
+  end
 end
 
 # Part 3
 
 class BookInStock
-# YOUR CODE HERE
+  attr_accessor :isbn , :price
+  
+  def initialize(isbn, price)
+    @isbn = isbn
+    @price = price
+    raise ArgumentError if @isbn.empty? || price <= 0
+  end
+  
+  def price_as_string
+    return "$%0.2f" % [@price]
+  end
+  
 end
